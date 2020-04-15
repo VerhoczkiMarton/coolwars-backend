a682561eb161630e0b78e763e4b3f996686ad65f
VerhoczkiMarton <verhoczki.marci@gmail.com>
2020-04-15 11:47:23 +0200

    Initial commit

---
+++ src/main/java/com/codecool/coolwarsbackend/CoolwarsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.coolwarsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoolwarsBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoolwarsBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codecool/coolwarsbackend/CoolwarsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecool.coolwarsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoolwarsBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
